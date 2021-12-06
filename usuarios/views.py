18317c10d0306d48948f887d3353bef21362925c
Rafael Witt <rafawitt@hotmail.com>
2021-12-06 14:37:58 -0300

    Create new func to verify fields

--- usuarios/views.py
+++ usuarios/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.models import User
-from django.contrib import auth
+from django.contrib import auth, messages
 from receitas.models import Receita
 
 def cadastro(request):
@@ -10,20 +10,29 @@ def cadastro(request):
         senha = request.POST['password']
         senha2 = request.POST['password2']
 
-        if not nome.strip():
+        if campo_vazio(nome):
+            messages.error(request, 'O campo email deve ser preenchido')
             return redirect('cadastro')
 
-        if not email.strip():
+        if campo_vazio(email):
+            messages.error(request, 'O campo email deve ser preenchido')
             return redirect('cadastro')
 
-        if senha != senha2:
+        if senha_diferente(senha, senha2):
+            messages.error(request, 'Senhas não conferem')
             return redirect('cadastro')
 
         if User.objects.filter(email=email).exists():
+            messages.error(request, 'Email já cadastrado')
+            return redirect('cadastro') 
+
+        if User.objects.filter(username=nome).exists():
+            messages.error(request, 'Nome já cadastrado')
             return redirect('cadastro') 
 
         user = User.objects.create_user(username=nome,email=email, password=senha)
         user.save()
+        messages.success(request, 'Cadastro realizado com sucesso')
 
         return redirect('login')
     else:
@@ -33,16 +42,19 @@ def login(request):
     if request.method == 'POST':
         email = request.POST['email']
         senha = request.POST['senha']
-        if email == "" or senha == "":
+        if campo_vazio(email) or campo_vazio(senha):
+            messages.error(request, 'Preencha os campos')
             return redirect('login')
         
-        if User.objects.filter(email=email).exists():
+        if verifica_usuario(email):
             nome = User.objects.filter(email=email).values_list('username', flat=True).get()
             user = auth.authenticate(request, username=nome, password=senha)
             if user is not None:
                 auth.login(request, user)
-
             return redirect('dashboard')
+        else:
+            messages.error(request, 'Dados invalidos')
+            return redirect('login')        
 
     return render(request, 'usuarios/login.html')
 
@@ -80,3 +92,12 @@ def cria_receita(request):
         return redirect('dashboard')
     else:
         return render(request, 'usuarios/cria_receita.html')
+
+def campo_vazio(campo):
+    return not campo.strip()
+
+def senha_diferente(senha, senha2):
+    return senha != senha2
+    
+def verifica_usuario(email):
+    return User.objects.filter(email=email).exists()
